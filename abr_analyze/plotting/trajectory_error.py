Minor bug fix in derivative calc of traj error
trajectory_error.py
- minor bug fix in indexing

plot_trajectory_error.py
- subplotting different orders of error

abr_analyze/plotting/trajectory_error.py
@@ -146,16 +146,19 @@ class TrajectoryError():
 
         # integrate data
         if self.time_derivative > 0:
+            # set our keys that are able to be differentiated to avoid errors
+            differentiable_keys = ['ee_xyz', 'ideal_trajectory']
+            if ideal is not None:
+                differentiable_keys.append(ideal)
+
             for key in data:
-                if key != 'time':
+                if key in differentiable_keys:
                     # differentiate the number of times specified by
                     # time_derivative
                     for _ in range(0, self.time_derivative):
-                        print(np.array(data[key][:, 0]).shape)
-                        print(np.mean(data['time']))
-                        data[key][:, 0] = np.gradient(data[key][:, .0], dt)
-                        data[key][:, 1] = np.gradient(data[key][:, .1], dt)
-                        data[key][:, 2] = np.gradient(data[key][:, .2], dt)
+                        data[key][:, 0] = np.gradient(data[key][:, 0], dt)
+                        data[key][:, 1] = np.gradient(data[key][:, 1], dt)
+                        data[key][:, 2] = np.gradient(data[key][:, 2], dt)
 
         # filter data
         if self.filter_const is not None:

examples/plot_trajectory_error.py
@@ -1,4 +1,5 @@
 from abr_analyze.plotting import TrajectoryError
+from abr_analyze.paths import figures_dir
 import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
@@ -7,56 +8,48 @@ import matplotlib.pyplot as plt
 db_name = 'dewolf2018neuromorphic'
 test_group = 'friction_post_tuning'
 test_list = [
-          'nengo_cpu_friction_102_0',
-          'nengo_cpu_friction_103_0',
-          'nengo_cpu_friction_104_0',
-          ]
+             'nengo_cpu_friction_102_0',
+             'nengo_cpu_friction_103_0',
+             'nengo_cpu_friction_104_0',
+             'nengo_loihi_friction_46_0',
+            ]
+label = [
+         ': 10k',
+         ': 1k',
+         ': 50k',
+         ': 50k',
+        ]
 sessions = 1
 runs = 50
-time_derivative=0
+time_derivatives = [0,1,2,3]
+derivative_print = ['0th', '1st', '2nd', '3rd']
 filter_const=None
 interpolated_samples=400
-traj = TrajectoryError(db_name=db_name,
-                       time_derivative=time_derivative,
-                       filter_const=filter_const,
-                       interpolated_samples=interpolated_samples)
+if len(time_derivatives)>3:
+    plt_dims = [2,int(np.ceil(len(time_derivatives))/2)]
+else:
+    plt_dims = [len(time_derivatives), 1]
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-c = ['k','b', 'g', 'r', 'y']
-for ii, test in enumerate(test_list):
-    print('Processing test %i/%i: %s'%(ii+1, len(test_list), test))
-    traj.statistical_error(save_location='%s/%s'%(test_group, test),
-            sessions=sessions, runs=runs)
-for ii, test in enumerate(test_list):
+fig = plt.figure(figsize=(12,8))
+for jj, td in enumerate(time_derivatives):
+    print('-- Processing %s derivative trajectory error --'%derivative_print[td])
+    title = 'trajectory_error_%i'%(td)
+    ax = fig.add_subplot(plt_dims[0], plt_dims[1], jj+1)
+    traj = TrajectoryError(db_name=db_name,
+                        time_derivative=td,
+                        filter_const=filter_const,
+                        interpolated_samples=interpolated_samples)
+    c = ['k','b', 'g', 'r', 'y']
+    for ii, test in enumerate(test_list):
+        print('Processing test %i/%i: %s'%(ii+1, len(test_list), test))
+        traj.statistical_error(save_location='%s/%s'%(test_group, test),
+                sessions=sessions, runs=runs)
     print('Plotting...')
-    traj.plot(ax=ax, save_location='%s/%s'%(test_group, test), label=test,
-            c=c[ii], loc=0)
-    ax.legend()
-# ax = [plt.subplot(211),
-#         plt.subplot(212)]
-# tenk_tests = [
-#               test_list[0],
-#               test_list[1],
-#               test_list[2],
-#               test_list[4],
-#               ]
-#
-# fiftyk_tests = [
-#               test_list[0],
-#               test_list[1],
-#               test_list[3],
-#               test_list[5],
-#               ]
-#
-# for ii, test in enumerate(tenk_tests):
-#     print('Plotting...')
-#     traj.plot(ax=ax[0], save_location='%s/%s'%(test_group, test), label=test,
-#             c=c[ii], loc=0, title='10k')
-# ax[0].legend(loc=2)
-# for ii, test in enumerate(fiftyk_tests):
-#     print('Plotting...')
-#     traj.plot(ax=ax[1], save_location='%s/%s'%(test_group, test), label=test,
-#             c=c[ii], loc=0, title='50k')
-# ax[1].legend(loc=2)
+    for ii, test in enumerate(test_list):
+        traj.plot(ax=ax, save_location='%s/%s'%(test_group, test), label=test+label[ii],
+                c=c[ii], loc=0, title=title)
+        ax.legend()
+loc = '%s/neuroarm/trajectory_error.png'%(figures_dir)
+print('Figure saved to %s'%(loc))
+plt.savefig(loc)
 plt.show()