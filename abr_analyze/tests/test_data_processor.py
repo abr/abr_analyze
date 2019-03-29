f28526915259cb859be75955dbdc293c0d4de48d
travis dewolf <travis.dewolf@gmail.com>
2019-03-29 17:32:07 -0400

    Test for data_processor load_and_process refactor
    - seems like there were supposed to be more tests written for this
      function?

--- abr_analyze/tests/test_data_processor.py
+++ abr_analyze/tests/test_data_processor.py
@@ -118,69 +118,93 @@ def test_scale_data(plt):
 #     test = 'test_filter_data()'
 #     results[test] = {}
 #     print('\n%s----------%s----------%s'%(BLUE, test, ENDC))
-#
-# def test_load_and_process():
-#     results = {}
-#     test = 'test_load_and_process()'
-#     results[test] = {}
-#     print('\n%s----------%s----------%s'%(BLUE, test, ENDC))
-#
-#     def load_and_process(db_name, save_location, parameters,
-#             interpolated_samples, test, label, default_pass, results,
-#             expected_len=None):
-#         try:
-#             passed = default_pass
-#             data = proc.load_and_process(db_name=db_name,
-#                 save_location=save_location, parameters=parameters,
-#                 interpolated_samples=interpolated_samples)
-#         except Exception as e:
-#             print('TEST: %s | SUBTEST: %s'%(test, label))
-#             print('%s%s%s'%(RED,e,ENDC))
-#             passed = not default_pass
-#             data = None
-#             results[test]['%s'%label] = passed
-#             return results, data
-#
-#         if expected_len is not None:
-#             for key in parameters:
-#                 if key == 'time':
-#                     key = 'cumulative_time'
-#                 if len(data[key]) != expected_len:
-#                     passed = not default_pass
-#                     results[test]['%s'%label] = passed
-#                     return results, data
-#
-#         results[test]['%s'%label] = passed
-#         return results, data
-#
-#     generate_random_traj(steps=147, plot=True)
-#
-# #CONTINUE FROM HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEERE
-#
-#     # cycle through to test with and without interpolated_samples
-#     # param doesn't exist
-#     # pass without time
-#     # pass with time
-#     # pass multiple with time
-#     loc = 'fake_trajectory'
-#     params = ['ee_xyz', 'ideal_trajectory', 'time']
-#     interpolated_samples = 50
-#     data = load_and_process(
-#         db_name=db,
-#         save_location=loc,
-#         parameters=params,
-#         interpolated_samples=interpolated_samples,
-#         test=test,
-#         label='param does not exist',
-#         default_pass=True,
-#         results=results,
-#         expected_len=interpolated_samples)
-#
-#     # pass multiple without time
-#
-#     ascii_table.print_params(title=None, data={'test': results[test]},
-#             invert=True)
-#
+
+
+def generate_random_traj(dat, steps, plot=False):
+    steps = steps
+    alpha = 0.7
+    ee_xyz = [[np.random.uniform(0.05, 0.2, 1),
+               np.random.uniform(0.05, 0.2, 1),
+               np.random.uniform(0.5, 1.0, 1)]]
+
+    for ii in range(0,steps):
+
+        if ii == 0:
+            xx = np.random.uniform(-2, 2, 1)/100
+            yy = np.random.uniform(-2, 2, 1)/100
+            zz = np.random.uniform(-2, 2, 1)/100
+            x = ee_xyz[-1][0] + xx
+            y = ee_xyz[-1][1] + yy
+            z = ee_xyz[-1][2] + zz
+
+        else:
+            xx = xx * alpha + np.random.uniform(-2, 2, 1)/100 * (1-alpha)
+            yy = yy * alpha + np.random.uniform(-2, 2, 1)/100 * (1-alpha)
+            zz = zz * alpha + np.random.uniform(-2, 2, 1)/100 * (1-alpha)
+            x = ee_xyz[-1][0] + xx
+            y = ee_xyz[-1][1] + yy
+            z = ee_xyz[-1][2] + zz
+
+        ee_xyz.append([x, y, z])
+
+    ee_xyz = np.squeeze(np.array(ee_xyz))
+
+    alpha = 0.2
+    ideal = np.zeros((steps+1, 3))
+    for ii, val in enumerate(ee_xyz.tolist()):
+
+        if ii == 0:
+            ideal[0] = val
+
+        else:
+            ideal[ii][0] = alpha*val[0] + (1-alpha)*ideal[ii-1][0]
+            ideal[ii][1] = alpha*val[1] + (1-alpha)*ideal[ii-1][1]
+            ideal[ii][2] = alpha*val[2] + (1-alpha)*ideal[ii-1][2]
+
+    ideal = np.array(ideal)
+    times = np.ones(steps+1) * 0.03 + np.random.rand(steps+1)/50
+
+    data = {'ee_xyz': ee_xyz, 'ideal_trajectory': ideal, 'time': times}
+    dat.save(data=data, save_location='fake_trajectory', overwrite=True)
+
+    if plot:
+        from mpl_toolkits.mplot3d import axes3d  # pylint: disable=W0611
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_title('Generated Trajectory')
+        ax.plot(ee_xyz[:,0], ee_xyz[:,1], ee_xyz[:,2], label='ee_xyz')
+        ax.plot(ideal[:,0], ideal[:,1], ideal[:,2], label='ideal')
+        ax.legend()
+        plt.show()
+
+
+def test_load_and_process():
+    dat = DataHandler('tests')
+
+    interpolated_samples = 50
+    parameters = ['ee_xyz', 'ideal_trajectory', 'time']
+    data = proc.load_and_process(
+        db_name='tests',
+        save_location='fake_trajectory',
+        parameters=parameters,
+        interpolated_samples=interpolated_samples,
+        )
+
+    for key in parameters:
+        if key == 'time':
+            key = 'cumulative_time'
+        assert len(data[key]) == interpolated_samples
+
+
+    generate_random_traj(dat, steps=147, plot=True)
+
+    # cycle through to test with and without interpolated_samples
+    # param doesn't exist
+    # pass without time
+    # pass with time
+    # pass multiple with time
+
+
 # def test_calc_cartesian_points():
 #     results = {}
 #     test = 'test_test_calc_cartesian_points()'
@@ -192,59 +216,3 @@ def test_scale_data(plt):
 #     test = 'test_two_norm_error()'
 #     results[test] = {}
 #     print('\n%s----------%s----------%s'%(BLUE, test, ENDC))
-#
-# def generate_random_traj(steps, plot=False):
-#     print('Generating fake trajectories...')
-#     steps = steps
-#     alpha = 0.7
-#     ee_xyz = [[np.random.uniform(0.05, 0.2, 1),
-#                np.random.uniform(0.05, 0.2, 1),
-#                np.random.uniform(0.5, 1.0, 1)]]
-#     for ii in range(0,steps):
-#         if ii == 0:
-#             xx = np.random.uniform(-2, 2, 1)/100
-#             yy = np.random.uniform(-2, 2, 1)/100
-#             zz = np.random.uniform(-2, 2, 1)/100
-#             x = ee_xyz[-1][0] + xx
-#             y = ee_xyz[-1][1] + yy
-#             z = ee_xyz[-1][2] + zz
-#         else:
-#             xx = xx * alpha + np.random.uniform(-2, 2, 1)/100 * (1-alpha)
-#             yy = yy * alpha + np.random.uniform(-2, 2, 1)/100 * (1-alpha)
-#             zz = zz * alpha + np.random.uniform(-2, 2, 1)/100 * (1-alpha)
-#             x = ee_xyz[-1][0] + xx
-#             y = ee_xyz[-1][1] + yy
-#             z = ee_xyz[-1][2] + zz
-#         ee_xyz.append([x, y, z])
-#
-#     ee_xyz = np.squeeze(np.array(ee_xyz))
-#
-#     alpha = 0.2
-#     ideal = np.zeros((steps+1, 3))
-#     for ii, val in enumerate(ee_xyz.tolist()):
-#         if ii == 0:
-#             ideal[0] = val
-#         else:
-#             ideal[ii][0] = alpha*val[0] + (1-alpha)*ideal[ii-1][0]
-#             ideal[ii][1] = alpha*val[1] + (1-alpha)*ideal[ii-1][1]
-#             ideal[ii][2] = alpha*val[2] + (1-alpha)*ideal[ii-1][2]
-#
-#     ideal = np.array(ideal)
-#     times = np.ones(steps+1) * 0.03 + np.random.rand(steps+1)/50
-#
-#     data = {'ee_xyz': ee_xyz, 'ideal_trajectory': ideal, 'time': times}
-#     dat.save(data=data, save_location='fake_trajectory', overwrite=True)
-#
-#     if plot:
-#         import matplotlib
-#         from mpl_toolkits.mplot3d import axes3d  # pylint: disable=W0611
-#         fig = plt.figure()
-#         ax = fig.add_subplot(111, projection='3d')
-#         ax.set_title('Generated Trajectory')
-#         ax.plot(ee_xyz[:,0], ee_xyz[:,1], ee_xyz[:,2], label='ee_xyz')
-#         ax.plot(ideal[:,0], ideal[:,1], ideal[:,2], label='ideal')
-#         ax.legend()
-#         plt.show()
-#
-#
-# db = 'tests'
