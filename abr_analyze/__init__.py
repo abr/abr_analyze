e8f019863c4818ea79ca42a3807d1ccb3e487313
p3jawors <p3jawors@gmail.com>
2020-09-14 11:55:08 -0400

    Generalized db_viewer
    - will plot datasets to figure
    - will break figure up into subplots
    - can print params in bash table with differences highlighted

--- abr_analyze/__init__.py
+++ abr_analyze/__init__.py
@@ -6,3 +6,12 @@ from . import nengo
 from . import gui
 
 from .data_handler import DataHandler
+
+import os
+
+dir_name = os.path.dirname(__file__)
+if not os.path.isfile:
+    with open("%s/paths.txt" % dir_name, "a+") as f:
+        f.write('cache_dir:\n')
+        f.write('database_dir:\n')
+        f.write('figures_dir:\n')
