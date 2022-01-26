4c9dd3223f86f43ba4a5f52a8d467e42f23b28a1
Pawel <p3jawors@gmail.com>
2022-01-26 12:30:31 -0500

    Updates to work with mujoco config
    - everything runs the same as before, but now we catch when we are
    using a mujoco config and call the corresponding functions

--- examples/save_npz_to_hdf5.py
+++ examples/save_npz_to_hdf5.py
@@ -1,8 +1,11 @@
 import numpy as np
 
 from abr_analyze.utils import npz_to_hdf5
-from download_examples_db import check_exists as examples_db
 
+# TODO the name of the npz file you are sampling
+save_name = "test.npz"
+# this is the 'folder' in the database that the data will be saved to
+test_name =  'test_0000'
 
 examples_db()
 a = np.ones(11)
@@ -14,4 +17,4 @@ npz_to_hdf5.convert(
     db_name="abr_analyze_examples",
     save_location="my_converted_data/test1",
     overwrite=True,
-)
+
